5a8824af7fc281ad75fa861ade3c1a1b8affca8e
kfamaguana <kfamaguana@espe.edu.ec>
2024-12-16 11:05:52 -0500

    Divsion Carpetas Parciales

---
+++ PARCIAL 2/Nueva carpeta/js/First.js
@@ -0,0 +1,23 @@
+var nombre = "Kevin";
+var apellido = "Amaguaña";
+var edad = 20;
+console.log("Mi nombre es " + nombre + " " + apellido + " tengo " + edad + " años");
+console.log("Mi edad es " + edad + " años");
+
+let x = 10;
+let y = 20;
+let z = x + y;
+console.log(z);
+
+//var = declara una variable global
+//let = declara una variable local o tambien una variable de bloque
+//const = declara una constante es decir que no va a cambiar su valor
+
+//if marca un bloque de codigo que se ejecuta si se cumple una condicion
+//switch marca un bloque de codigo que se ejecuta si se cumple una condicion
+//for marca un bloque de codigo que se ejecuta si se cumple una condicion
+//fuccion marca un bloque de instrucciones que se ejecutan varias veces se declara
+//try se ejecuta cuando se produce un error
+//catch se ejecuta cuando se produce un error
+//return devuelve un valor
+//break sale de un bucle
